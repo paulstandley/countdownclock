3a81559d40d70ae43110063f9a48f3bd969accc4
Paul Standley <paulstandley1972@gmail.com>
2019-10-12 15:19:05 +0100

    add date to dom

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 "use strict";
 
-let title = "Hollyday";
-
+let title = "Hollyday in";
+let clockDate = Date();
+console.log(clockDate);
+// get dom elements h2 section
 const h2Title = document.getElementById("h2Id");
+const dateClock = document.getElementById("clockId");
 
 h2Title.innerText = title;
-
+dateClock.innerText = clockDate;
 console.log(h2Title);
+
