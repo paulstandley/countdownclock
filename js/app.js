957deaf86d8f1f430b5dea60722306d0833434fa
Paul Standley <paulstandley1972@gmail.com>
2019-10-12 20:14:35 +0100

    add form input

--- js/app.js
+++ js/app.js
@@ -2,26 +2,38 @@
 
 let title = "Hollyday starts";
 let clockDate = new Date();
-let birthdayHollday = new Date('November 2, 2019 15:05:00');
-let date1 = birthdayHollday.toDateString();
+let dateStringTarget = new Date('November 2, 2019 15:05:00');
+let dateString = dateStringTarget.toDateString();
 
-console.log(date1);
-console.dir(clockDate);
-// get dom elements h2 section
+// get dom elements
 const targetTitle = document.getElementById("targetTitle");
 const dateClock = document.getElementById("clockId");
 const targetDate = document.getElementById("targetDate");
+
+// get form values
+
+newTitle.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const newTitle = document.getElementById("newTitle");
+  const newDate = document.getElementById("newDate");
+  console.log(newTitle);
+  console.log(newDate);
+  console.log(evt);
+}, false);
+
 // add to dom
 targetTitle.innerText = title;
 dateClock.innerText = clockDate;
-targetDate.innerText = date1;
+targetDate.innerText = dateString;
 
+// update current time
 setInterval(function() {  
-let clockDate = new Date();
-let dateYear = new Date().getFullYear();
-//console.dir(clockDate);
-// add to dom
-targetTitle.innerText = `${title}`;
-dateClock.innerText = clockDate;
+  let upDate = new Date();
+  targetTitle.innerText = `${title}`;
+  dateClock.innerText = upDate;
 }, 1000);
-console.log(targetTitle);
+
+
+console.log(clockDate);
+console.log(newTitle);
+console.log(newDate);
