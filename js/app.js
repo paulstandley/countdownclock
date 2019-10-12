55df28f1342af475c386eeeeed49f4002267efc5
Paul Standley <paulstandley1972@gmail.com>
2019-10-12 16:42:22 +0100

    set interval to update dom

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 "use strict";
 
 let title = "Hollyday in";
-let clockDate = Date();
-console.log(clockDate);
+let clockDate = new Date();
 // get dom elements h2 section
 const h2Title = document.getElementById("h2Id");
 const dateClock = document.getElementById("clockId");
-
+// add to dom
 h2Title.innerText = title;
 dateClock.innerText = clockDate;
-console.log(h2Title);
 
+setInterval(function() {  
+let clockDate = new Date();
+let dateYear = new Date().getFullYear();
+//console.dir(clockDate);
+// add to dom
+h2Title.innerText = title;
+dateClock.innerText = clockDate;
+}, 1000);
+console.log(h2Title);
