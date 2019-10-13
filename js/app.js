b3b1f5ae42ead1cc7c5a0430bbc1b05e20dbbc77
Paul Standley <paulstandley1972@gmail.com>
2019-10-13 20:51:46 +0100

    add calculate to diff template

--- js/app.js
+++ js/app.js
@@ -9,15 +9,12 @@ let dateString = dateStringTarget.toDateString();
 const targetTitle = document.getElementById("targetTitle");
 const dateClock = document.getElementById("clockId");
 const targetDate = document.getElementById("targetDate");
+const diff = document.getElementById('diff');
 
 // get form values
-
-newTitle.addEventListener('click', (evt) => {
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (evt) => {
   evt.preventDefault();
-  const newTitle = document.getElementById("newTitle");
-  const newDate = document.getElementById("newDate");
-  console.log(newTitle);
-  console.log(newDate);
   console.log(evt);
 }, false);
 
@@ -29,10 +26,22 @@ targetDate.innerText = dateString;
 // update current time
 setInterval(function() {  
   let upDate = new Date();
+  let time = new Date().getTime();
+  let nexttime = new Date('November 2, 2019 15:05:00').getTime();
+  diff.innerHTML = `<p>milliseconds :${calculate(time, nexttime)}</p>
+    <p>seconds : ${Math.floor(calculate(time, nexttime) / 1000)}
+    <p>minutes : ${Math.floor((calculate(time, nexttime) / 1000) / 60)}</p>
+    <p>hours : ${Math.floor(((calculate(time, nexttime) / 1000) / 60) / 60)}</p>
+    <p>days : ${Math.floor((((calculate(time, nexttime) / 1000) / 60) / 60) / 24)}</p>
+    `;
   targetTitle.innerText = `${title}`;
   dateClock.innerText = upDate;
-}, 1000);
+}, 100);
 
+// calculate difference between two dates
+function calculate(time, nexttime) {
+  return nexttime - time;
+}
 
 console.log(clockDate);
 console.log(newTitle);
