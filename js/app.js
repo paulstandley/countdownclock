9ff55042701eff74201e9ffe03c2a00bccbbedb5
Paul Standley <paulstandley1972@gmail.com>
2019-10-18 20:42:40 +0100

    clean up and al ok

--- js/app.js
+++ js/app.js
@@ -1,52 +1,62 @@
 "use strict";
 
-let title = "Hollyday starts";
-let clockDate = new Date();
-let dateStringTarget = new Date('November 2, 2019 15:05:00');
-let dateString = dateStringTarget.toDateString();
+runtime();
 
-// get dom elements
-const targetTitle = document.getElementById("targetTitle");
-const dateClock = document.getElementById("clockId");
-const targetDate = document.getElementById("targetDate");
-const diff = document.getElementById('diff');
-
-// get form values
-const submit = document.getElementById('submit');
+let inputValueArray = [];
 
 function submitInfo() {
   const newTitle = document.getElementById("newTitle").value;
   const newDate = document.getElementById("newDate").value;
-  console.log(newTitle, newDate);
-  let thing = new Date(newDate).getTime();
-  console.log(thing);
+  const newTime = document.getElementById("newTime").value;
+  let newDateTime = new Date(`${newDate}, ${newTime}`).getTime();
+  targetTitle.innerText = newTitle;
+  console.log(newDateTime);
 }
 
-// add to dom
-targetTitle.innerText = title;
-dateClock.innerText = clockDate;
-targetDate.innerText = dateString;
-
 // update current time
-setInterval(function() {  
-  let upDate = new Date();
-  let time = new Date().getTime();
-  let nexttime = new Date('November 2, 2019 15:05:00').getTime();
-  diff.innerHTML = `<p>milliseconds :${calculate(time, nexttime)}</p>
-    <p>seconds : ${Math.floor(calculate(time, nexttime) / 1000)}
-    <p>minutes : ${Math.floor((calculate(time, nexttime) / 1000) / 60)}</p>
-    <p>hours : ${Math.floor(((calculate(time, nexttime) / 1000) / 60) / 60)}</p>
-    <p>days : ${Math.floor((((calculate(time, nexttime) / 1000) / 60) / 60) / 24)}</p>
+function runtime() {
+
+  let title = "Hollyday starts";
+  let clockDate = new Date();
+  let dateStringTarget = new Date('November 2, 2019 15:05:00');
+  let dateString = dateStringTarget.toDateString();
+
+  // get dom elements
+  const targetTitle = document.getElementById("targetTitle");
+  const dateClock = document.getElementById("clockId");
+  const targetDate = document.getElementById("targetDate");
+  const display = document.getElementById('display');
+
+  let upDate, time, nexttime, milliseconds, seconds, minutes, hours, days;
+
+  setInterval(() => {
+    upDate = new Date();
+    time = new Date().getTime();
+    nexttime = new Date('November 2, 2019 15:05:00').getTime();
+    milliseconds = calculate(time, nexttime);
+    seconds = calculate(time, nexttime) / 1000;
+    minutes = (calculate(time, nexttime) / 1000) / 60;
+    hours = ((calculate(time, nexttime) / 1000) / 60) / 60;
+    days = (((calculate(time, nexttime) / 1000) / 60) / 60) / 24;
+    display.innerHTML = `
+      <p>milliseconds :${milliseconds}</p>
+      <p>seconds : ${Math.floor(seconds)}
+      <p>minutes : ${Math.floor(minutes)}</p>
+      <p>hours : ${Math.floor(hours)}</p>
+      <p>days : ${Math.floor(days)}</p>
     `;
-  targetTitle.innerText = `${title}`;
-  dateClock.innerText = upDate;
-}, 100);
+    dateClock.innerText = upDate;
+  }, 100);
+    
+  // add to dom
+  targetTitle.innerText = title;
+  dateClock.innerText = clockDate;
+  targetDate.innerText = dateString;
+  targetTitle.innerText = title;
+
+}
 
 // calculate difference between two dates
 function calculate(time, nexttime) {
   return nexttime - time;
 }
-
-console.log(clockDate);
-console.log(newTitle);
-console.log(newDate);
